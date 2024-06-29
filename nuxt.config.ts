4b7db2ee51432b702b05bc483276ff2368ede872
Balazs Barabas <barabas.balazska@gmail.com>
2024-06-29 17:38:53 +0300

    project scaffold, with basic marquee animation

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@nuxt/eslint"],
+  typescript: {
+    typeCheck: true,
+  },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>The Index Page</div>
+    <div class="border-2 border-red-500">
+        <MarqueeAnimation :items="technologyItems" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { MarqueeLink } from '~/types/marquee-link';
+
+const technologyItems = [
+    {
+        link: "https://vuejs.org/",
+        logo: "/app/icons/vue.svg"
+    },
+    {
+        link: "https://fastify.dev/",
+        logo: "/app/icons/fastify.svg"
+    }
+] as MarqueeLink[];
+
+</script>
