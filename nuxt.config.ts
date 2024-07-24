0725f681c72f414618518fd710d53e865982459e
Balazs Barabas <barabas.balazska@gmail.com>
2024-07-24 16:13:14 +0300

    added repo slug

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: "/personal-site/",
+  },
   devtools: { enabled: true },
   modules: ["@nuxt/eslint", "@nuxt/test-utils/module"],
   typescript: {
