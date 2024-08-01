2c7119b409c83a0b274de09ce424cb8be1f028b1
Balazs Barabas <barabas.balazska@gmail.com>
2024-08-01 13:57:02 +0300

    added OCR project and necessary items

--- constants/project-items.ts
+++ constants/project-items.ts
@@ -1,6 +1,7 @@
 import cartOutline from "~/assets/app/icons/cart-outline.svg";
 import mapLegend from "~/assets/app/icons/map-legend.svg";
 import denoWhite from "~/assets/app/logos/deno-white.svg";
+import ocr from "~/assets/app/icons/ocr.svg";
 
 export interface OptionsItem {
   text: string;
@@ -29,9 +30,24 @@ export const projectItems = [
     link: "https://b2b.bickleyandmitchell.com",
   },
   {
+    key: "ocr",
+    title: "Optical Character Recognition",
+    subtitle: "Handwritten digit classification model",
+    description:
+      "Optical Character Recognition (OCR) model for converting images of handwritten digits into machine-readable data using k-nearest neighbors (k-NN) and centroid-based classification methods. ",
+    img: ocr,
+    link: "https://github.com/Barabasbalazs/OCR",
+    options: [
+      {
+        text: "View source code",
+        link: "https://github.com/Barabasbalazs/OCR",
+      },
+    ] as OptionsItem[],
+  },
+  {
     key: "mapapp",
-    title: "Map App",
-    subtitle: "Hiking App with real time Tracking",
+    title: "Map based hiking Application",
+    subtitle: "Full Stack app with real time Tracking",
     description:
       "An intuitive web app with real-time tracking, helping tourists and guides explore and navigate both natural landscapes and urban areas with ease and efficiency.",
     img: mapLegend,
