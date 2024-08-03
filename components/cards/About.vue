a7d5ae3d602b9fccabd40d70fcf24fd410ce418f
Balazs Barabas <barabas.balazska@gmail.com>
2024-08-03 13:28:21 +0300

    about section desktop rework

--- components/cards/About.vue
+++ components/cards/About.vue
@@ -1,31 +1,44 @@
 <template>
-  <NuxtLayout name="card">
-    <div class="flex flex-col justify-between h-full gap-3 relative">
-      <OptionsButton />
+  <div class="h-full p-6">
+    <div class="flex flex-col gap-10">
       <div class="flex flex-col gap-3">
         <h3 class="uppercase">Full Stack Developer</h3>
         <div class="flex text-4xl gap-3">
           <h1 class="font-black">Barabás</h1>
           <h1 class="font-semibold">Balázs</h1>
         </div>
+      </div>
+      <div class="flex flex-col gap-8 text-stone-400">
         <p>
           {{ description }}
         </p>
+        <NuxtLink
+          :to="gmapQuerry"
+          class="text-stone-400 underline-link"
+          target="_blank"
+          >{{ coordinates.displayValue }}</NuxtLink
+        >
+        <div class="flex text-lg gap-6">
+          <p v-for="(language, index) in languages" :key="index">
+            {{ language }}
+          </p>
+        </div>
+        <div class="flex gap-8 text-sm">
+          <OptionsRedirect
+            v-for="(option, index) in options"
+            :key="index"
+            :option="option"
+          />
+        </div>
       </div>
-      <NuxtLink
-        :to="gmapQuerry"
-        class="text-stone-400 underline-link"
-        target="_blank"
-        >{{ coordinates.displayValue }}</NuxtLink
-      >
     </div>
-  </NuxtLayout>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { aboutItem } from "~/constants/about-items";
 
-const { coordinates, description, options } = aboutItem;
+const { coordinates, description, options, languages } = aboutItem;
 
 provide("options", {
   optionsValues: options,
@@ -34,6 +47,6 @@ provide("options", {
 
 const gmapQuerry = computed(
   () =>
-    `https://www.google.com/maps/search/?api=1&query=${coordinates?.numericalValue?.lat}%2C+${coordinates?.numericalValue?.lng}`,
+    `https://www.google.com/maps/search/?api=1&query=${coordinates?.numericalValue?.lat}%2C+${coordinates?.numericalValue?.lng}`
 );
 </script>
